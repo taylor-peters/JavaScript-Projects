06c8bb40a5e4a295e3284b495ebd5de312599133
Taylor Peters <taylordanielpeters@gmail.com>
2021-11-17 14:28:33 -0800

    Commented Code

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,25 +1,25 @@
 function winAlert() {
-    window.alert("Window Alert");
+    window.alert("Window Alert");  //Executes a window alert.
 }
 
 function concVar() {
     var x = "Variable 1", y = "Variable 2";
-    result = (x + " " + y);
+    result = (x + " " + y);  //Concatenates variables and adds a space between them.
     document.getElementById("Conc").innerHTML = result;
 }
 
 function fontColor() {
     var x = "Colored Text";
-    result = x.fontcolor("Red")
+    result = x.fontcolor("Red")  //Colors var x as red
     document.getElementById("Color").innerHTML = result;
 }
 
 function exp() {
-    result = (3 * 9);
+    result = (3 * 9);  //Solves expression and sets result
     document.getElementById("Expression").innerHTML = result;
 }
 
 function docWrite() {
-    document.write("Document Write");
+    document.write("Document Write");  //Writes to the screen
 }
 
