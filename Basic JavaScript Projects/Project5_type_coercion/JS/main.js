5b7c87bd4956f86d9af09c202e2275e1bfb0bac9
Taylor Peters <taylordanielpeters@gmail.com>
2021-11-17 15:02:46 -0800

    Additional logical expressions and operators

---
+++ Basic JavaScript Projects/Project5_type_coercion/JS/main.js
@@ -0,0 +1,18 @@
+function my_Function() {
+    document.getElementById("Test").innerHTML = 0 / 0;
+}
+function my_Function2() {
+    document.getElementById("Test2").innerHTML = isNaN("test");
+}
+function my_Function3() {
+    document.getElementById("Test3").innerHTML = isNaN(7);
+}
+function my_Function4() {
+    document.getElementById("Test4").innerHTML = 2E310;
+}
+function my_Function5() {
+    document.getElementById("Test5").innerHTML = -2E310;
+}
+
+
+
