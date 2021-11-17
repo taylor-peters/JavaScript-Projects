f364e2b2a326bd4bb8cbfb4f0b183e832525a199
Taylor Peters <taylordanielpeters@gmail.com>
2021-11-17 15:15:01 -0800

    early ternary operators

--- Basic JavaScript Projects/Project5_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_type_coercion/JS/main.js
@@ -1,3 +1,10 @@
+function Ride_Function() {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short" : "You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+}
+
 function my_Function() {
     document.getElementById("Test").innerHTML = 0 / 0;
 }
